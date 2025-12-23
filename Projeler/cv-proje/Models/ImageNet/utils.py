8d9922294902e1045b9bc15456891f62d4ee3b7e
İbrahim Mert <102045520+immertt@users.noreply.github.com>
2025-12-23 19:36:35 +0300

    Add function to load model weights from file

--- Projeler/cv-proje/Models/ImageNet/utils.py
+++ Projeler/cv-proje/Models/ImageNet/utils.py
@@ -1,1 +1,6 @@
+import torch
 
+def load_weights(model, weight_path):
+    state_dict = torch.load(weight_path, map_location="cpu")
+    model.load_state_dict(state_dict)
+    print("Model ağırlıkları yüklendi.")
