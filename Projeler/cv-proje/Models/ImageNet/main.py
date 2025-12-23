d65dc88e34209ad8c9b78dbe513e3dd6253f322b
İbrahim Mert <102045520+immertt@users.noreply.github.com>
2025-12-23 19:40:22 +0300

    Add main function for training and testing model

--- Projeler/cv-proje/Models/ImageNet/main.py
+++ Projeler/cv-proje/Models/ImageNet/main.py
@@ -1,1 +1,27 @@
+import argparse
+import config
+from dataset import get_dataloaders
+from model import build_model
+from train import train_model
+from eval import test_model
+from utils import load_weights
 
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--mode", choices=["train", "test"], required=True)
+    parser.add_argument("--weights", type=str, help="Model ağırlık yolu")
+    args = parser.parse_args()
+
+    train_loader, val_loader, test_loader = get_dataloaders()
+    model = build_model()
+
+    if args.mode == "train":
+        train_model(model, train_loader, val_loader)
+        test_model(model, test_loader)
+
+    elif args.mode == "test":
+        load_weights(model, args.weights)
+        test_model(model, test_loader)
+
+if __name__ == "__main__":
+    main()
