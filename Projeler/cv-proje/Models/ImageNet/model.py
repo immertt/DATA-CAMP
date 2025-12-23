9d2abc8897b9eed1748e2c8d7f6953d801d28c50
İbrahim Mert <102045520+immertt@users.noreply.github.com>
2025-12-23 19:35:26 +0300

    Implement model building function with ResNet options
    Added a function to build models based on configuration.

--- Projeler/cv-proje/Models/ImageNet/model.py
+++ Projeler/cv-proje/Models/ImageNet/model.py
@@ -1,1 +1,16 @@
+import torch.nn as nn
+from torchvision import models
+import config
 
+def build_model():
+    if config.MODEL_NAME == "resnet18":
+        model = models.resnet18(pretrained=config.PRETRAINED)
+    elif config.MODEL_NAME == "resnet50":
+        model = models.resnet50(pretrained=config.PRETRAINED)
+    else:
+        raise ValueError("Desteklenmeyen model")
+
+    in_features = model.fc.in_features
+    model.fc = nn.Linear(in_features, config.NUM_CLASSES)
+
+    return model
